3d8c86210c8aa36bee8ff2922c9f7105835816a2
Alexei <alexei.nezhyborets@onix-systems.com>
2013-11-16 16:10:40 +0200

    NSData + MD5 add+modif

---
+++ NSData+MD5.h
@@ -0,0 +1,13 @@
+//
+//  NSData+MD5.h
+//  MagidGlove
+//
+//  Created by Alexei on 13.11.13.
+//  Copyright (c) 2013 Orainteractive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (MD5)
+- (NSString *)MD5;
+@end
