00d19556b32a8b50fd601460d9aa20f83382971c
Alexei <alexei.nezhyborets@onix-systems.com>
2013-11-19 20:01:34 +0200

    remove debug constant from NAMHelper

--- NAMHelper.h
+++ NAMHelper.h
@@ -19,9 +19,6 @@ typedef void (^nam_integerCompletionBlock)(NSInteger intValue);
 
 @interface NAMHelper : NSObject
 
-#warning DEBUG
-#define DEBUG 1
-
 #ifdef DEBUG
 #define DLog( s, ... ) NSLog( @"<%p %@:(%d)> %@", self, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
 #else
