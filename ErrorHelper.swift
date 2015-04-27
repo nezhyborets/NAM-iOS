fb74180a307399079dbbd5c5738251fccf27fd3f
Nezhyborets Oleksii <alexei.nezhyborets@onix-systems.com>
2015-04-28 00:06:30 +0300

    some fixes

--- ErrorHelper.swift
+++ ErrorHelper.swift
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 Onix. All rights reserved.
 //
 
-let kErrorDomain = NSBundle.mainBundle().bundleIdentifier != nil ? NSBundle.mainBundle().bundleIdentifier! : "defaultDomain"
-
 let kDataFormatError = 1
 let kNotLoggedInErrorCode = 2
 let kNoStoredApiKeyErrorCode = 2
@@ -19,6 +17,12 @@ extension NSError {
 }
 
 class ErrorHelper: NSObject {
+    private static let kErrorDomain = NSBundle.mainBundle().bundleIdentifier != nil ? NSBundle.mainBundle().bundleIdentifier! : "defaultDomain"
+    
+    class func appErrorDomain() -> String {
+        return kErrorDomain
+    }
+    
     class func notLoggedInError() -> NSError {
         return NSError(domain: kErrorDomain, code: kNotLoggedInErrorCode, userInfo: [NSLocalizedDescriptionKey : "Not logged in"])
     }
@@ -41,23 +45,45 @@ class ErrorHelper: NSObject {
             
             if let uError = requestError {
                 errorForCompletion = uError
-            } else if let errorString = responseJson?["error"].string {
+            } else if let errorString = responseJson?["error"] as? String {
                 println("error found in json \(responseJson)")
-                errorForCompletion = NSError(domain: "Swayy", code: 0, userInfo: [NSLocalizedDescriptionKey : errorString])
+                errorForCompletion = NSError(domain: "Debts", code: 0, userInfo: [NSLocalizedDescriptionKey : errorString])
             }
             
             completionBlock(error: errorForCompletion)
         }
     }
     
-    class func defaultSearchingOfErrorForRequest(json: JSON?, error: NSError?) -> NSError? {
+    class func errorForRequest(json: [NSObject : AnyObject]?, error: NSError?) -> NSError? {
         return self.errorForRequest(json, successKey: nil, error: error)
     }
     
-    class func errorForRequest(json: JSON?, successKey: String?, error: NSError?) -> NSError? {
+    class func errorForRequest(json: [NSObject : AnyObject]?, successKey: String?, error: NSError?) -> NSError? {
         if let uError = error {
             return uError
-        } else if let errorString = json?["error"].string {
+        } else if let errorString = json?["error"] as? String {
+            if errorString == "Not logged in" || errorString == "Not logged in"  {
+                println(json)
+                return ErrorHelper.notLoggedInError()
+            } else {
+                println(json)
+                return ErrorHelper.errorWithMessage(errorString)
+            }
+        } else if let key = successKey {
+            if json?[key] == nil {
+                return ErrorHelper.errorWithMessage("Unknown error")
+            }
+        }
+        
+        return nil
+    }
+    
+    class func errorForRequest(json: [NSObject : AnyObject]?, statusCode: NSInteger, error: NSError?, successKey: String?) -> NSError? {
+        if statusCode == 401 {
+            return ErrorHelper.notLoggedInError()
+        } else if let uError = error {
+            return uError
+        } else if let errorString = json?["error"] as? String {
             if errorString == "Not logged in" {
                 println(json)
                 return ErrorHelper.notLoggedInError()
@@ -74,25 +100,27 @@ class ErrorHelper: NSObject {
         return nil
     }
     
-    class func textForError(error: NSError) -> String? {
+    class func textForError(error: NSError, statusCode: NSInteger) -> String? {
         var errorString: String?
         
-        if error.code == -1001 {
-            errorString = "Request timed out"
-        } else if error.code == -1009 {
-            errorString = "Not connected to the internet"
-        } else if error.code == 3840 {
-            errorString = "Unknown server error"
-        } else if error.code == -1003 {
-            errorString = "Cannot find host."
-        } else if error.code == -1005 {
-            errorString = "Network connection was lost."
+        if error.domain == (kCFErrorDomainCFNetwork as String) {
+            if error.code == -1001 {
+                errorString = "Request timed out"
+            } else if error.code == -1009 {
+                errorString = "Not connected to the internet"
+            } else if error.code == 3840 {
+                errorString = "Unknown server error"
+            } else if error.code == -1003 {
+                errorString = "Cannot find host."
+            } else if error.code == -1005 {
+                errorString = "Network connection was lost."
+            }
         }
         
         return errorString
     }
     
-    class func userFriendlyErrorForNetworkError(error: NSError) -> NSError {
+    class func userFriendlyErrorForNetworkError(error: NSError, statusCode: NSInteger) -> NSError {
         var userInfo: [NSObject : AnyObject] = [:]
         if let uErrorInfo = error.userInfo {
             userInfo = uErrorInfo
@@ -102,7 +130,7 @@ class ErrorHelper: NSObject {
         
         var userInfoM = userInfo
         if userInfo[NSLocalizedDescriptionKey] == nil {
-            let errorString = self.textForError(error)
+            let errorString = self.textForError(error, statusCode: statusCode)
             
             if let string = errorString {
                 userInfoM[NSLocalizedDescriptionKey] = string
@@ -111,12 +139,16 @@ class ErrorHelper: NSObject {
         
         return NSError(domain: error.domain, code: error.code, userInfo: userInfoM)
     }
-
-    class func showErrorWithMessage(message: String, cancelButtonTitle: String, otherButtonTitles: [String], completion: (alertView: UIAlertView?, buttonIndex: Int) -> ()) {
-        UIAlertView.showWithTitle("Oops!", message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: otherButtonTitles, tapBlock: completion)
+    
+    class func userFriendlyErrorForNetworkError(error: NSError) -> NSError {
+        return self.userFriendlyErrorForNetworkError(error, statusCode: NSNotFound)
     }
 
-    class func showErrorWithMessage(message: String, completion: (alertView: UIAlertView?, buttonIndex: Int) -> ()) {
-        UIAlertView.showWithTitle("Oops!", message: message, cancelButtonTitle: "Ok", otherButtonTitles: nil, tapBlock: completion)
-    }
+//    class func showErrorWithMessage(message: String, cancelButtonTitle: String, otherButtonTitles: [String], completion: (alertView: UIAlertView?, buttonIndex: Int) -> ()) {
+//        UIAlertView.showWithTitle("Oops!", message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: otherButtonTitles, tapBlock: completion)
+//    }
+//
+//    class func showErrorWithMessage(message: String, completion: (alertView: UIAlertView?, buttonIndex: Int) -> ()) {
+//        UIAlertView.showWithTitle("Oops!", message: message, cancelButtonTitle: "Ok", otherButtonTitles: nil, tapBlock: completion)
+//    }
 }
