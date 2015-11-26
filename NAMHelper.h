2458af4a912a2032e5c8343581d25387c394f043
Nezhyborets Oleksii <alexei.nezhyborets@onix-systems.com>
2015-11-26 13:26:27 +0200

    system version to h

--- NAMHelper.h
+++ NAMHelper.h
@@ -36,6 +36,12 @@ FOUNDATION_EXPORT NSInteger const CECodeFacebookCancelled;
 
 FOUNDATION_EXPORT NSInteger const CECodeChangeIsNotMade;
 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
 @interface NAMHelper : NSObject
 
 #ifdef DEBUG
