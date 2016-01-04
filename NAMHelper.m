fd5fafe6d5b15a5d8ab054fb460f6da556407665
Nezhyborets Oleksii <alexei.nezhyborets@onix-systems.com>
2016-01-04 16:09:41 +0200

    incread email valid characters after dot from 4 to 6

--- NAMHelper.m
+++ NAMHelper.m
@@ -221,7 +221,7 @@ BOOL nam_stringExistsAndFilledBool(id object) {
 #pragma mark - Validation
 
 BOOL emailIsValid(NSString *candidate) {
-    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
     NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
 
     return [emailTest evaluateWithObject:candidate];
