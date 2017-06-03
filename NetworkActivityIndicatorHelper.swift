803b3f4bbde9962fb002c4a0c7d28cd88cee2e64
nezhyborets <am.nezhyborets@gmail.com>
2017-06-03 18:40:09 +0300

    Copyright fix

--- NetworkActivityIndicatorHelper.swift
+++ NetworkActivityIndicatorHelper.swift
@@ -3,7 +3,7 @@
 //  Swayy
 //
 //  Created by Oleksii Nezhyborets on 03.03.15.
-//  Copyright (c) 2015 Onix. All rights reserved.
+//  Copyright (c) 2015 Nezhyborets. All rights reserved.
 //
 
 import UIKit
